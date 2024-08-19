76ec881696a3c3c4566eda4d9d51ffc07a99c278
Minoru Tomobe <minoru.tomobe@gmail.com>
2024-08-20 01:43:11 +0900

    for Dify 0.7.1

--- voicevox/tts/sota_translation.py
+++ voicevox/tts/sota_translation.py
@@ -1,245 +1,96 @@
 import logging
 import MeCab
 import ipadic
+import os
 import re
 
-translation_table = {
-    '2m': 'ツーメーター',
-    '6m': 'シックスメーター',
-    '10m': 'テンメーター',
-    '15m': 'じゅうごメーター',
-    '20m': 'にじゅうメーター',
-    '40m': 'よんじゅうメーター',
-    '80m': 'はちじゅうメーター',
-    '160m': 'ひゃくろくじゅうメーター',
-    'achievement': 'アチーブメント',
-    'activation' : 'アクティベーション',
-    'activations' : 'アクティベーション',
-    'activator' : 'アクティベーター',
-    'activators' : 'アクティベーター',
-    'account': 'アカウント',
-    'add' : 'アド',
-    'adif' : 'エイディフ',
-    'air': 'エアー',
-    'alert' : 'アラート',
-    'alerts': 'アラート',
-    'alexloop': 'アレックスループ',
-    'alltrails': 'オールトレイルズ',
-    'and' : 'アンド',
-    'antenna': 'アンテナ',
-    'antennas': 'アンテナ',
-    'anywhere': 'エニィフェア',
-    'app': 'アップ',
-    'award' : 'アワード',
-    'awards': 'アワード',
-    'band': 'バンド',
-    'bandspringer': 'バンドスプリンガー',
-    'bronze': 'ブロンズ',
-    'buddipole': 'バディポール',
-    'buddistick': 'バディスティック',
-    'by': 'バイ',
-    'callsign': 'コールサイン',
-    'certificate': 'サーティフィケイト',
-    'certificates': 'サーティフィケイト',
-    'chaser' : 'チェイサー',
-    'chasers' : 'チェイサー',
-    'chasing': 'チェイシング',
-    'call' : 'コール',
-    'calling' : 'コーリング',
-    'comment': 'コメント',
-    'comments': 'コメント',
-    'complete': 'コンプリート',
-    'create': 'クリエイト',
-    'chameleon': 'カメレオン',
-    'choose': 'チューズ',
-    'cluster' : 'クラスター',
-    'coil': 'コイル',
-    'coils': 'コイルズ',
-    'database' : 'データベース',
-    'date': 'デート',
-    'defined': 'デファインド',
-    'deluxe': 'デラックス',
-    'dipole': 'ダイポール',
-    'discovery': 'ディスカバリー',
-    'elecraft': 'エレクラフト',
-    'end': 'エンド',
-    'fed': 'フェッド',
-    'end-fedz': 'エンドフェッズ',
-    'enter': 'エンター',
-    'entry' : 'エントリ',
-    'explorer': 'エクスプローラー',
-    'fast': 'ファスト',
-    'file': 'ファイル',
-    'friendly': 'フレンドリー',
-    'gaia' : 'ガイア',
-    'goat': 'ゴート',
-    'gold': 'ゴールド',
-    'ground': 'グラウンド',
-    'half': 'ハーフ',
-    'ham' : 'ハム',
-    'hamalert': 'ハムアラート',
-    'hamlog': 'ハムログ',
-    'happy': 'ハッピー',
-    'hole' : 'ホール',
-    'honour' : 'オナー',
-    'hopper': 'ホッパー',
-    'icom': 'アイコム',
-    'ii': 'ツー',
-    'iii': 'スリー',
-    'is' : 'イズ',
-    'it': 'イット',
-    'kenwood':'ケンウッド',
-    'labs': 'ラボラトリーズ',
-    'log' : 'ログ',
-    'logbook': 'ログブック',
-    'logs' : 'ログ',
-    'logger': 'ロガー',
-    'logging': 'ロギング',
-    'loop': 'ループ',
-    'map': 'マップ',
-    'mapping': 'マッピング',
-    'mountain': 'マウンテン',
-    'modular': 'モジュラー',
-    'milestone': 'マイルストン',
-    'milestones': 'マイルストン',
-    'mini': 'ミニ',
-    'mkii': 'マークツー',
-    'mkiii': 'マークスリー',
-    'my' : 'マイ',
-    'myantennas': 'マイアンテナズ',
-    'official' : 'オフィシャル',
-    'other' : 'アザー',
-    'package': 'パッケージ',
-    'packages': 'パッケージズ',
-    'packtenna': 'パックテナ',
-    'par': 'パー',
-    'park': 'パーク',
-    'parks': 'パークス',
-    'plane': 'プレーン',
-    'point' : 'ポイント',
-    'points' : 'ポイント',
-    'portable': 'ポータブル',
-    'pota':'ポタ',
-    'precision': 'プレシジョン',
-    'project': 'プロジェクト',
-    'propagaion': 'プロパゲーション',
-    'radio': 'ラディオ',
-    'reference': 'リファレンス',
-    'references': 'リファレンス',
-    'register': 'レジスター',
-    'river': 'リバー',
-    'roll' : 'ロール',
-    's2s': 'エスツーエス',
-    'save': 'セーブ',
-    'shack': 'シャック',
-    'silver': 'シルバー',
-    'single': 'シングル',
-    'site' : 'サイト',
-    'sites': 'サイト',
-    'sloth': 'スロース',
-    'software': 'ソフトウェア',
-    'sota' : 'ソタ',
-    'sotabeams': 'ソタビームス',
-    'sotawatch' : 'ソタウォッチ',
-    'spot' : 'スポット',
-    'spots': 'スポット',
-    'spotting' : 'スポッティング',
-    'spotter': 'スポッター',
-    'standing': 'スタンディング',
-    'submit': 'サブミット',
-    'summit': 'サミット',
-    'summits': 'サミット',
-    'summits on the air' : 'サミットオンジエアー',
-    'super': 'スーパー',
-    'system': 'システム',
-    'systems': 'システム',
-    'take': 'テイク',
-    'the' : 'ジ',
-    'this': 'ディス',
-    'trail': 'トレイル',
-    'upload': 'アップロード',
-    'vagabond': 'バガボンド',
-    'vertical': 'バーチカル',
-    'walkham': 'ウォークハム',
-    'watch': 'ウォッチ',
-    'website' : 'ウェブサイト',
-    'wolf': 'ウォルフ',
-    'xiegu': 'シェーグー',
-    'yaesu': 'ヤエス',
-    'yamap': 'ヤマップ',
-    'zone': 'ゾーン',
-    '##': ' ',
-    '###': ' ',
-    '####': ' ',
-    ',': ', ',
-    '--': ' ',
-    '---': ' ',
-    '----': ' ',
-    '**': '',
-}
 
 logger = logging.getLogger(__name__)
 
-chunk_buffer = ""
 
-def sota_translation(content: str) -> str:
-    global chunk_buffer
-    mecab = MeCab.Tagger(ipadic.MECAB_ARGS)
-    content = re.sub(r'\*\*','',content)
-    content , chunk_buffer = remove_urls_from_chunk(content, chunk_buffer)
-    content_lines = content.split('\n')
-    translation_result = []
-    for line in content_lines:
-        nodes = []
-        node = mecab.parseToNode(line)
-        prev = None
-        while node:
-            s:str = node.surface 
-            if s.isdigit():
-                prev = s
-                node = node.next
-                continue
-            if prev:
-                if s == 'm':
-                    alt = int(prev)
-                    if alt >= 150 and alt <= 3776:
-                        nodes.append(prev + 'メートル')
-                    else:
-                        nodes.append(prev + s)
+class TTStranslation:
+    chunk_buffer = ""
+
+    def __init__(self, table_name="default_translation.txt"):
+        self.logger = logging.getLogger(__name__)
+        self.mecab = MeCab.Tagger(ipadic.MECAB_ARGS)
+        self.translation_table = {}
+        path = os.path.dirname(os.path.abspath(__file__))
+        if table_name:
+            try:
+                with open(os.path.join(path, table_name)) as f:
+                    translist = f.readlines()
+                    logger.warning(f"read translation rules = {translist}")
+            except Exception as e:
+                translist = []
+
+            for rule in translist:
+                lhsrhs = rule.split(":")
+                if len(lhsrhs) > 1:
+                    self.translation_table[lhsrhs[0]] = lhsrhs[1].strip()
                 else:
-                    nodes.append(prev)
+                    self.translation_table[lhsrhs[0]] = ""
+
+    def sota_translation(self, content: str) -> str:
+        content = re.sub(r"\*\*", "", content)
+        content = self.remove_urls_from_chunk(content)
+        content_lines = content.split("\n")
+        translation_result = []
+
+        for line in content_lines:
+            nodes = []
+            node = self.mecab.parseToNode(line)
+            prev = ""
+            while node:
+                s: str = node.surface
+                if s.isdigit():
+                    prev += s
+                    node = node.next
+                    continue
+                if prev:
+                    if s == "m":
+                        alt = int(prev)
+                        if alt >= 150 and alt <= 3776:
+                            nodes.append(prev + "メートル")
+                        else:
+                            nodes.append(prev + s)
+                    else:
+                        nodes.append(prev)
+                        nodes.append(s)
+                    prev = ""
+                    node = node.next
+                    continue
+                else:
                     nodes.append(s)
-                prev = None
-                node = node.next
-                continue
-            else:
-                nodes.append(s)
-                node = node.next
+                    node = node.next
 
-        replaced_line = [translation_table.get(node.lower(), node) for node in nodes if node]
-        #logger.warning(f"chunk replaced = {replaced_line}")
-        translation_result.append(''.join(replaced_line))
+            replaced_line = [
+                self.translation_table.get(node.lower(), node) for node in nodes if node
+            ]
+            self.logger.warning(f"chunk replaced = {replaced_line}")
+            translation_result.append("".join(replaced_line))
+        result = "\n ".join(translation_result)
+        self.logger.warning(f"translation result = {content} ->\n{result}")
+        return result
 
-    result = '\n '.join(translation_result)
-    #logger.warning(f"translation result = {content} ->\n{result}")
-    return result
-
-def remove_urls_from_chunk(chunk, buffer=""):
-    url_pattern = re.compile(r'https?:[^\s()]+')
-    combined = buffer + re.sub(r'（|）',' ',chunk)
-    matches = list(url_pattern.finditer(combined))
-    if matches:
-        cleaned_chunk = url_pattern.sub(r'', combined)
-        removed = 0
-        for m in matches:
-            start, end = m.span()
-            if start > len(buffer):
-                break
-            if end > len(buffer):
-                removed += len(buffer) - start
-            else:
-                removed += end - start
-        cleaned_chunk = cleaned_chunk[len(buffer)-removed:]
-        return cleaned_chunk, chunk
-    else:
-        return chunk, ""
+    def remove_urls_from_chunk(self, chunk: str) -> str:
+        url_pattern = re.compile(r"https?:[^\s()]+")
+        combined = self.chunk_buffer + re.sub(r"（|）", " ", chunk)
+        matches = list(url_pattern.finditer(combined))
+        if matches:
+            cleaned_chunk = url_pattern.sub(r"", combined)
+            removed = 0
+            for m in matches:
+                start, end = m.span()
+                if start > len(self.chunk_buffer):
+                    break
+                if end > len(self.chunk_buffer):
+                    removed += len(self.chunk_buffer) - start
+                else:
+                    removed += end - start
+            cleaned_chunk = cleaned_chunk[len(self.chunk_buffer) - removed :]
+            self.chunk_buffer = chunk
+            return cleaned_chunk
+        else:
+            self.chunk_buffer = ""
+            return chunk
--- voicevox/tts/tts.py
+++ voicevox/tts/tts.py
@@ -12,46 +12,85 @@ from core.model_runtime.errors.invoke import (
     InvokeServerUnavailableError,
 )
 
-from core.model_runtime.model_providers.voicevox.tts.sota_translation import sota_translation
+from core.model_runtime.model_providers.voicevox.tts.sota_translation import (
+    TTStranslation,
+)
+
 
 class VoicevoxText2SpeechModel(TTSModel):
     """
     Model class for VOICEVOX Speech to text model.
     """
 
-    def _invoke(self, model: str, tenant_id: str, credentials: dict,
-                content_text: str, voice: str, user: Optional[str] = None) -> any:
-        if not voice or voice not in [d["value"] for d in self.get_tts_model_voices(model=model, credentials=credentials)]:
-            voice = self._get_model_default_voice(model, credentials)
-        return self._tts_invoke(model=model, credentials=credentials, content_text=content_text, voice=voice)
+    def __init__(self):
+        self.tts_translation = TTStranslation()
 
-    def validate_credentials(self, model: str, credentials: dict, user: Optional[str] = None) -> None:
+    def _invoke(
+        self,
+        model: str,
+        tenant_id: str,
+        credentials: dict,
+        content_text: str,
+        voice: str,
+        user: Optional[str] = None,
+    ) -> any:
+        if not voice or voice not in [
+            d["value"]
+            for d in self.get_tts_model_voices(model=model, credentials=credentials)
+        ]:
+            voice = self._get_model_default_voice(model, credentials)
+        return self._tts_invoke(
+            model=model, credentials=credentials, content_text=content_text, voice=voice
+        )
+
+    def validate_credentials(
+        self, model: str, credentials: dict, user: Optional[str] = None
+    ) -> None:
         try:
-            next(self._tts_invoke(
-                model=model,
-                credentials=credentials,
-                content_text="こんにちは。",
-                voice=self._get_model_default_voice(model, credentials),
-            ))
+            next(
+                self._tts_invoke(
+                    model=model,
+                    credentials=credentials,
+                    content_text="こんにちは。",
+                    voice=self._get_model_default_voice(model, credentials),
+                )
+            )
         except Exception as ex:
             raise CredentialsValidateFailedError(str(ex))
 
-    def _tts_invoke(self, model: str, credentials: dict, content_text: str, voice: str) -> any:
+    def _tts_invoke(
+        self, model: str, credentials: dict, content_text: str, voice: str
+    ) -> any:
         audio_type = self._get_model_audio_type(model, credentials)
         word_limit = self._get_model_word_limit(model, credentials)
         max_workers = self._get_model_workers_limit(model, credentials)
         try:
-            sentences = list(self._split_text_into_sentences(org_text=sota_translation(content_text), 
-                                                             pattern='。',
-                                                             max_length=word_limit))
+            sentences = list(
+                self._split_text_into_sentences(
+                    org_text=self.tts_translation.sota_translation(content_text),
+                    pattern="。",
+                    max_length=word_limit,
+                )
+            )
             # Create a thread pool and map the function to the list of sentences
-            with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-                futures = [executor.submit(self._process_sentence, sentence=sentence, voice=voice,
-                                           api_base=credentials["voicevox_api_base"]) for sentence in sentences]
+            with concurrent.futures.ThreadPoolExecutor(
+                max_workers=max_workers
+            ) as executor:
+                futures = [
+                    executor.submit(
+                        self._process_sentence,
+                        sentence=sentence,
+                        voice=voice,
+                        api_base=credentials["voicevox_api_base"],
+                    )
+                    for sentence in sentences
+                ]
                 for future in futures:
                     if future.result():
                         buffer: BytesIO = BytesIO()
-                        segment = AudioSegment.from_file(BytesIO(future.result()), format=audio_type)
+                        segment = AudioSegment.from_file(
+                            BytesIO(future.result()), format=audio_type
+                        )
                         segment.export(buffer, format="mp3")
                         buffer.seek(0)
                         yield buffer.read()
@@ -60,12 +99,21 @@ class VoicevoxText2SpeechModel(TTSModel):
 
     def _process_sentence(self, sentence: str, voice: str, api_base: str):
         with httpx.Client() as client:
-            query_resp = client.post(api_base + "/audio_query", params={"speaker": voice, "text": sentence.strip()},timeout=30.0)
+            query_resp = client.post(
+                api_base + "/audio_query",
+                params={"speaker": voice, "text": sentence.strip()},
+                timeout=30.0,
+            )
             audio_query = query_resp.json()
-            audio_resp = client.post(api_base + "/synthesis", params={"speaker": voice}, json=audio_query, timeout=30.0)
+            audio_resp = client.post(
+                api_base + "/synthesis",
+                params={"speaker": voice},
+                json=audio_query,
+                timeout=30.0,
+            )
             if isinstance(audio_resp.content, bytes):
                 return audio_resp.content
-     
+
     @property
     def _invoke_error_mapping(self) -> dict[type[InvokeError], list[type[Exception]]]:
         # TODO: Break down the errors
